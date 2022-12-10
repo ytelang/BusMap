37b9effd1cf1f096bbce409f76205a19eaeeba46
YashTelang <ytelang11@gmail.com>
2022-12-10 23:50:45 +0000

    Graph structure

---
+++ src/adj_list.h
@@ -0,0 +1,336 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <set>
+#include <queue>
+#include <list>
+#include <utility>
+#include <algorithm>
+#include <cstdlib>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+
+typedef std::vector<std::vector<std::string> > V2D; 
+typedef std::string Vertex;
+/**
+ * @file edge.h
+ * Definition and (minimal) implementation of an edge class.
+ */
+
+using std::string;
+/**
+ * Represents an edge in a graph; used by the Graph class.
+ *
+ * @author Sean Massung
+ * @date Spring 2012
+ */
+class Edge
+{
+  public:
+    Vertex source; /**< The source of the edge **/
+    Vertex dest; /**< The destination of the edge **/
+
+    /**
+     * Parameter constructor for unweighted graphs.
+     * @param u - one vertex the edge is connected to
+     * @param v - the other vertex it is connected to
+     */
+    Edge(Vertex u, Vertex v)
+        : source(u), dest(v), label(""), weight(-1)
+    { /* nothing */
+    }
+
+    /**
+     * Parameter constructor for unweighted graphs.
+     * @param u - one vertex the edge is connected to
+     * @param v - the other vertex it is connected to
+     * @param lbl - the edge label
+     */
+    Edge(Vertex u, Vertex v, string lbl)
+        : source(u), dest(v), label(lbl), weight(-1)
+    { /* nothing */
+    }
+
+    /**
+     * Parameter constructor for weighted graphs.
+     * @param u - one vertex the edge is connected to
+     * @param v - the other vertex it is connected to
+     * @param w - the weight of the edge
+     * @param lbl - the edge label
+     */
+    Edge(Vertex u, Vertex v, int w, string lbl)
+        : source(u), dest(v), label(lbl), weight(w)
+    { /* nothing */
+    }
+
+    /**
+     * Default constructor.
+     */
+    Edge() : source(""), dest(""), label(""), weight(-1)
+    { /* nothing */
+    }
+
+    /**
+     * Compares two Edges.
+     * operator< is defined so Edges can be sorted with std::sort.
+     * @param other - the edge to compare with
+     * @return whether the current edge is less than the parameter
+     */
+    bool operator<(const Edge& other) const
+    {
+        return weight < other.weight;
+    }
+
+    /**
+     * Gets edge label.
+     */
+    string getLabel() const
+    {
+        return this->label;
+    }
+
+    /**
+     * Gets edge weight.
+     */
+    int getWeight() const
+    {
+        return this->weight;
+    }
+
+    /**
+     * Compares two edges' source and dest.
+     * @param other - the edge to compare with
+     */
+    bool operator==(Edge& other) const
+    {
+        if (this->source != other.source)
+            return false;
+        if (this->dest != other.dest)
+            return false;
+        return true;
+    }
+private:
+    string label; /**< The edge label **/
+    int weight; /**< The edge weight (if in a weighed graph) **/
+
+};
+
+
+/**
+ * @file graph.h
+ * Graph Library Declarations
+ *
+ * Written for CS 225 Spring 2011
+ * @author Sean Massung
+ *
+ * Updated Spring 2012 by Sean Massung
+ *  - Added doxygen comments
+ *  - Created better error handling
+ *  - More encapsulated and object-oriented
+ * 
+ *  Updated Spring 2018 by Jordi
+ *  - Added doxygen comments
+ *  - Created better error handling
+ *  - More encapsulated and object-oriented
+ *
+ * Update Spring 18 by Simeng
+ * - Finishing adding all required features
+ */
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::set;
+using std::string;
+using std::to_string;
+using std::vector;
+using std::pair;
+using std::make_pair;
+using std::unordered_map;
+
+
+/**
+ * Represents a graph; used by the GraphTools class.
+ *
+ */
+class Graph
+{
+public:
+    /**
+     * Constructor to create an empty graph.
+     * @param weighted - specifies whether the graph is a weighted graph or
+     *  not
+     */
+    Graph(bool weighted);
+
+    /**
+     * Constructor to create an empty graph.
+     * @param weighted - specifies whether the graph is a weighted graph or
+     *  not
+     * @param directed - specifies whether the graph is directed
+     */
+    Graph(bool weighted, bool directed);
+
+    /**
+     * Gets all adjacent vertices to the parameter vertex.
+     * @param source - vertex to get neighbors from
+     * @return a vector of vertices
+     */
+    vector<Vertex> getAdjacent(Vertex source) const;
+
+
+    /**
+     * Gets all vertices in the graph.
+     * @return a vector of all vertices in the graph
+     */
+    vector<Vertex> getVertices() const;
+    
+
+    /**
+     * Gets an edge between two vertices.
+     * @param source - one vertex the edge is connected to
+     * @param destination - the other vertex the edge is connected to
+     * @return - if exist, return the corresponding edge
+     *         - if edge doesn't exist, return Edge()
+     */
+    Edge getEdge(Vertex source, Vertex destination) const;
+
+    /**
+     * Gets all the edges in the graph.
+     * @return a vector of all the edges in the graph
+     */
+    vector<Edge> getEdges() const;
+
+    /**
+     * Checks if the given vertex exists.
+     * @return - if Vertex exists, true
+     *         - if Vertex doesn't exist, return false
+     */
+    bool vertexExists (Vertex v) const;
+
+    /**
+     * Checks if edge exists between two vertices exists.
+     * @return - if Edge exists, true
+     *         - if Edge doesn't exist, return false
+     */
+    bool edgeExists(Vertex source, Vertex destination) const;
+
+        /**
+     * Sets the edge label of the edge between vertices u and v.
+     * @param source - one vertex the edge is connected to
+     * @param destination - the other vertex the edge is connected to
+     * @return - if edge exists, set the label to the corresponding edge(if not directed, set the reverse one too), return edge with new label
+     *         - if edge doesn't exist, return InvalidEdge
+     */
+        Edge setEdgeLabel(Vertex source, Vertex destination, string label);
+
+    /**
+     * Gets the edge label of the edge between vertices u and v.
+     * @param source - one vertex the edge is connected to
+     * @param destination - the other vertex the edge is connected to
+     * @return - if edge exists, return edge label
+     *         - if edge doesn't exist, return InvalidLabel
+     */
+    string getEdgeLabel(Vertex source, Vertex destination) const;
+
+    /**
+     * Gets the weight of the edge between two vertices.
+     * @param source - one vertex the edge is connected to
+     * @param destination - the other vertex the edge is connected to
+     * @return - if edge exists, return edge wright
+     *         - if doesn't, return InvalidWeight
+     */
+    int getEdgeWeight(Vertex source, Vertex destination) const;
+
+    /**
+     * Inserts a new vertex into the graph and initializes its label as "".
+     * @param v - the name for the vertex
+     */
+    void insertVertex(Vertex v);
+
+    /**
+     * Removes a given vertex from the graph.
+     * @param v - the vertex to remove
+     * @return - if v exists, return v
+     *         - if not, return InvalidVertex;
+     */
+    Vertex removeVertex(Vertex v);
+
+    /**
+     * Inserts an edge between two vertices.
+     * A boolean is returned for use with the random graph generation.
+     * Hence, an error is not thrown when it fails to insert an edge.
+     * @param source - one vertex the edge is connected to
+     * @param destination - the other vertex the edge is connected to
+     * @return whether inserting the edge was successful
+     */
+    bool insertEdge(Vertex source, Vertex destination);
+
+    /**
+     * Removes the edge between two vertices.
+     * @param source - one vertex the edge is connected to
+     * @param destination - the other vertex the edge is connected to
+     * @return - if edge exists, remove it and return removed edge
+     *         - if not, return InvalidEdge
+     */
+    Edge removeEdge(Vertex source, Vertex destination);
+
+    /**
+     * Sets the weight of the edge between two vertices.
+     * @param source - one vertex the edge is connected to
+     * @param destination - the other vertex the edge is connected to
+     * @param weight - the weight to set to the edge
+     * @return - if edge exists, set edge weight and return  edge with new weight
+     *         - if not, return InvalidEdge
+     */
+    Edge setEdgeWeight(Vertex source, Vertex destination, int weight);
+
+
+    bool isDirected() const;
+
+    void clear();
+
+
+    const static Vertex InvalidVertex;
+    const static Edge InvalidEdge;
+    const static int InvalidWeight;
+    const static string InvalidLabel;
+
+private:
+    mutable unordered_map<Vertex, unordered_map<Vertex, Edge>> adjacency_list;
+
+    bool weighted;
+    bool directed;
+
+
+    /**
+     * Returns whether a given vertex exists in the graph.
+     * @param v - the vertex to check
+     * @param functionName - the name of the calling function to return
+     *  in the event of an error
+     */
+    bool assertVertexExists(Vertex v, string functionName) const;
+
+    /**
+     * Returns whether thee edge exists in the graph.
+     * @param source - one vertex
+     * @param destination - another vertex
+     * @param functionName - the name of the calling function to return
+     *  in the event of an error
+     */
+    bool assertEdgeExists(Vertex source, Vertex destination, string functionName) const;
+
+
+    /**
+     * Prints a graph error and quits the program.
+     * The program is exited with a segfault to provide a stack trace.
+     * @param message - the error message that is printed
+     */
+    void error(string message) const;
+};
